2e1a850dc6c46c63cd02d7346153803975d42052
Sebastian Illa <seba.illa.99@gmail.com>
2023-01-06 18:03:55 -0300

    Placing orders (firestore II)

--- Sebastian-Illa/src/assets/Conteiners/CartContainer.jsx
+++ Sebastian-Illa/src/assets/Conteiners/CartContainer.jsx
@@ -1,10 +1,47 @@
-import React, { useContext } from 'react'
+
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useCartContext } from '../../context/CartContext'
+import { addDoc, collection, doc, getFirestore, updateDoc } from "firebase/firestore"
+
+
 
 const CartContainer = () => {
 
+  const [dataForm, SetFormData] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
   const { cartList, vaciarCarrito, precioTotal, eliminarPorItem } = useCartContext()
+
+  const addOrder = (e) => {
+    e.preventDefault()
+    const order = {}
+    order.buyer = dataForm 
+    order.precio = precioTotal()
+    order.item = cartList.map (({id, precio, name}) => ({id, precio, name}))
+  
+    const db = getFirestore()
+    const queryCollection = collection (db, 'orders')
+  
+
+    addDoc(queryCollection, order)
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+    .finally(() => vaciarCarrito())
+    
+  }
+const handleOnChange = (e) => {
+
+  SetFormData( {
+    ...dataForm,
+    [e.target.name]: e.target.value
+  })
+}
+
+
 
 
   return (
@@ -22,6 +59,36 @@ const CartContainer = () => {
         }
                               <h4>Valor Total : {precioTotal()}</h4>
                               <button className = "btn btn-danger" onClick = {vaciarCarrito}> Vaciar Carrito </button>
+
+                              <form onSubmit={addOrder}>
+                              <input 
+                              type="text"
+                              onChange={handleOnChange}
+                              name='name'
+                              value={dataForm.value}
+                              placeholder='Ingrese su Nombre'
+                              />
+                              <input 
+                              type="text"
+                              onChange={handleOnChange}
+                              name='phone'
+                              value={dataForm.phone}
+                              placeholder='Ingrese su Telefono'
+                              />
+                              <input 
+                              type="text"
+                              onChange={handleOnChange}
+                              name='email'
+                              value={dataForm.email}
+                              placeholder='Ingrese su Email'
+                              />
+                              <button className='btn btn-outline-success'>Terminar Compra</button>
+
+
+
+
+
+                              </form>
       </>
       :
         <>
