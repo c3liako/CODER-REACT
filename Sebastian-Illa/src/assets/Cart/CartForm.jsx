8ee6a14d0cc2a202b0cc19b01e4f2c37cf570e13
Sebastian Illa <seba.illa.99@gmail.com>
2023-02-11 19:56:54 -0300

    Al fin terminado

--- Sebastian-Illa/src/assets/Cart/CartForm.jsx
+++ Sebastian-Illa/src/assets/Cart/CartForm.jsx
@@ -1,15 +1,83 @@
 
 
-import { useState, memo } from 'react'
+import { useState, useEffect, memo } from 'react'
 import { useCartContext } from '../../context/CartContext'
 import { addDoc, collection, getFirestore,} from "firebase/firestore"
 
 
 const CartForm = memo(() => {
-    const [dataForm, SetFormData] = useState({
-    })
-
+    const [dataForm, SetFormData] = useState({ name: '', lastName: '', email: '', confirmEmail: '', phone: '' })
+    const [errorForm, setErrorForm] = useState({ name: '', email: '', confirmEmail: '', phone: '' })
     const { cartList, emptyCart, totalPrice} = useCartContext()
+
+
+
+
+
+    
+
+const handleOnChange = (e) => {
+
+    SetFormData( {
+    ...dataForm,
+    [e.target.name]: e.target.value
+    })
+}
+
+const validateForm = () => {
+    const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/
+    const regexTel = /^\d{10}$/
+    let error = {}
+
+    if (!dataForm.name.trim()) {
+        error.name = 'Se requiere un Nombre'
+    }
+
+    if (!dataForm.lastName.trim()) {
+        error.name = 'Se requiere un Apellido'
+    }
+
+    if (!dataForm.name.trim() && !dataForm.lastName.trim()) {
+        error.nombre = 'Se requiere Nombre y Apellido'
+    }
+
+    if (!dataForm.email.trim()) {
+        error.email = 'Se requiere un Email'
+    }
+
+    if (dataForm.email.trim() && !regexEmail.test(dataForm.email.trim())) {
+        error.email = 'Email no valido, revise'
+    }
+
+    if (!dataForm.confirmEmail.trim()) {
+        error.confirmEmail = 'Se requiere confirmar email'
+    }
+
+    if (dataForm.confirmEmail.trim() && !regexEmail.test(dataForm.confirmEmail.trim())) {
+        error.confirmEmail = 'Email no valido, revise'
+    }
+
+    if (dataForm.confirmEmail.trim() && dataForm.email.trim() && dataForm.confirmEmail.trim() !== dataForm.email.trim()) {
+        error.confirmEmail = 'Los email no son iguales verifique'
+    }
+
+    if (!dataForm.phone.trim()) {
+        error.phone = 'Se requiere un telefono'
+    }
+
+    if (dataForm.phone.trim() && !regexTel.test(dataForm.phone.trim())) {
+        error.phone = 'El numero debe tener 10 dígitos'
+    }
+
+    setErrorForm(error)
+}
+
+
+useEffect(() => {
+    validateForm()
+}, [dataForm])
+
+
 
     const addOrder = (e) => {
         e.preventDefault()
@@ -31,13 +99,11 @@ const CartForm = memo(() => {
     
     
     }
-const handleOnChange = (e) => {
 
-    SetFormData( {
-    ...dataForm,
-    [e.target.name]: e.target.value
-    })
-}
+
+
+
+
     return (
         <div 
             style={{
@@ -61,10 +127,22 @@ const handleOnChange = (e) => {
         onChange={handleOnChange}
         name="name"
         value={dataForm.name}
-        placeholder="Tu nombre completo"
+        placeholder="Nombre"
         required
     />
     </div>
+    <div className="m-3">
+    <input
+        className="form-control"
+        type="text"
+        onChange={handleOnChange}
+        name="lastName"
+        value={dataForm.lastName}
+        placeholder="Apellido"
+        required
+    />
+    </div>
+    {errorForm.name && <sup className='ps-2'>{errorForm.name}</sup>}
     <div className="m-3">
     <input
         className="form-control"
@@ -76,6 +154,7 @@ const handleOnChange = (e) => {
         required
     />
     </div>
+    {errorForm.phone && <sup className='ps-2'>{errorForm.phone}</sup>}
     <div className="m-3">
     <input
         className="form-control"
@@ -87,6 +166,19 @@ const handleOnChange = (e) => {
         required
     />
     </div>
+    {errorForm.email && <sup className='ps-2'>{errorForm.email}</sup>}
+    <div className="m-3">
+    <input
+        className="form-control"
+        type="text"
+        onChange={handleOnChange}
+        name="confirmEmail"
+        value={dataForm.confirmEmail}
+        placeholder="tucorreo@ejemplo.com"
+        required
+    />
+    </div>
+    {errorForm.confirmEmail && <sup className='ps-2'>{errorForm.confirmEmail}</sup>}
     <button className="btn bg-info p-3 m-3 fw-bolder">
     Finalizar Compra
     </button>
